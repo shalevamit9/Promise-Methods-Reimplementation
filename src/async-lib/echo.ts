c118f0a4002af7cf479f11ec03ff078dbac82770
Amit Shalev <amits@rapyd.ac>
2021-12-22 22:27:06 +0200

    finished echo

--- src/async-lib/echo.ts
+++ src/async-lib/echo.ts
@@ -1,9 +1,6 @@
-import log from '@ajar/marker';
 import { delay } from './delay';
 
 export const echo = async (msg: string, ms: number) => {
-  log.yellow(`--> start ${msg}`);
   await delay(ms);
-  log.blue(`finish <-- ${msg}`);
   return msg;
 };
